8eee33f3af9369ade762927a46cd087d11a0fce0
Casey L. Jones <caseylawsonjones@yahoo.com>
2017-09-13 14:57:06 -0400

    Initial commit

---
+++ obj/Release/Package/PackageTmp/Assets/scripts/post template/JavaScriptExercises.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+    $('#FactorialLink').click(function () {
+        var url = $('#Factorial').data('url');
+        $.get(url, function (data) {
+            $('#Factorial').html(data);
+            $('#Factorial').modal();
+        });
+    });
+});
+
